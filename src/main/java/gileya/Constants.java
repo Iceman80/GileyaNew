89dcc6ab90017d96f3b0ac2432b285105593ae8f
serhii.yakovlev <ysa.sergey@gmail.com>
2017-01-10 18:07:25 +0200

    I've changed findText class

--- src/main/java/gileya/Constants.java
+++ src/main/java/gileya/Constants.java
@@ -7,7 +7,8 @@ public final class Constants {
     public static final String FILE_NAME_FOR_SYTE = "Gileya.php";
     public static final String FILE_NAME_FOR_BIBLIO = "ZMIST.txt";
     public static final String CONTENT_TYPE_DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-    public static final String DIRECTORY = "C:\\Users\\Serhii\\Desktop\\Зміст.С.С\\";
+    //    public static final String DIRECTORY = "C:\\Users\\Serhii\\Desktop\\Зміст.С.С\\";
+    public static final String DIRECTORY = "C:\\Users\\serhii.yakovlev\\Desktop\\Зміст.С.С\\";
 
     public static final String SITE_HEADER = "<em><strong>ЗМІСТ - CONTENT </strong></em>\n" + "[cut]<br /></div>";
     public static final String BIBIO_HEADER = "\"Гілея: науковий вісник\" Збірник наукових праць, 2016, Вип. 11 ().\n" +
@@ -22,5 +23,6 @@ public final class Constants {
     public static final String PATTERN_NAME_AND_SURNAME = "[\\D{3,19}^\\s]\\s\\D{0,5}\\.\\s";
     //Поиск по названию темы
     public static final String PATTERN_THEMES = "^\\D{4,15}\\s\\D{4,15}$";
+    public static final String PATTERN_TITLE = "^\\D{4,15}$";
 
 }
--- src/main/java/gileya/FindText.java
+++ src/main/java/gileya/FindText.java
@@ -6,6 +6,11 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static gileya.Constants.PATTERN_FULL_NAME;
+import static gileya.Constants.PATTERN_NAME_AND_SURNAME;
+import static gileya.Constants.PATTERN_THEMES;
+import static gileya.Constants.PATTERN_TITLE;
+
 /*
 regexp for library
  */
@@ -15,66 +20,56 @@ public class FindText {
     public void findReplace(List<Scanner> scanners) {
         Scanner scannerUa = scanners.get(0);
         Scanner scannerEn = scanners.get(1);
-        String endEn;
         while (scannerUa.hasNext() && scannerEn.hasNext()) {
             String stUa = scannerUa.nextLine();
             String stEn = scannerEn.nextLine();
+            Pattern paternT = Pattern.compile(PATTERN_THEMES); //Поиск названия темы
+            Matcher matUaTheme = paternT.matcher(stUa);
+            Matcher matEnTheme = paternT.matcher(stEn);
+            Pattern paternTitle = Pattern.compile(PATTERN_TITLE); //Поиск названия темы
+            Matcher matUaTitle = paternTitle.matcher(stUa);
+            Matcher matEnTitle = paternTitle.matcher(stEn);
 
-            if (!stUa.equals("ЗМІСТ") && !stUa.equals("Історичні науки") && !stUa.equals("Філософські науки") &&
-                    !stUa.equals("Політичні науки") && !stUa.equals("Наукове життя")) {
-                Pattern patern1 = Pattern.compile("[А-Я,І,Є,A-Z]\\.\\s[А-Я,A-Z,І,Є]\\.\\s"); //Поиск инициалов имени и отчества Ua
+            if (!matUaTheme.find() && !matEnTheme.find() && !matUaTitle.find() && !matEnTitle.find()) {
+                Pattern patern1 = Pattern.compile(PATTERN_FULL_NAME); //Поиск инициалов имени и отчества Ua
                 Matcher mat1 = patern1.matcher(stUa);
-                Pattern patern1En = Pattern.compile("[A-Z,А-Я]\\.\\s[A-Z,А-Я]\\.\\s"); //Поиск инициалов имени и отчества En
+                Pattern patern1En = Pattern.compile(PATTERN_FULL_NAME); //Поиск инициалов имени и отчества En
                 Matcher mat1En = patern1En.matcher(stEn);
-
-                Pattern patern2 = Pattern.compile("[А-Я,A-Z,І,Є]\\.\\s");//Поиск инициалов имени без отчества Ua
+                Pattern patern2 = Pattern.compile(PATTERN_NAME_AND_SURNAME);//Поиск инициалов имени без отчества Ua
                 Matcher mat2 = patern2.matcher(stUa);
-                Pattern patern2En = Pattern.compile("[A-Z,А-Я]\\.\\s");//Поиск инициалов имени без отчества En
+                Pattern patern2En = Pattern.compile(PATTERN_NAME_AND_SURNAME);//Поиск инициалов имени без отчества En
                 Matcher mat2En = patern2En.matcher(stEn);
-                Pattern patern3En = Pattern.compile("[A-Z,А-Я][a-z,а-я]\\.\\s");//Поиск инициалов имени 2 знака без отчества En
-                Matcher mat3En = patern3En.matcher(stEn);
 
                 if (mat1.find()) {
                     String start = stUa.substring(0, mat1.end());
                     String endUa = stUa.substring(mat1.end());
                     if (mat1En.find()) {
-                        endEn = stEn.substring(mat1En.end());
-                        String fin = start + "[" + endUa + "]{" + endEn + "}";
-                        text.add(fin);
+                        createAndAddSting(start, endUa, stEn, mat1En);
                     } else if (mat2En.find()) {
-                        endEn = stEn.substring(mat2En.end());
-                        String fin = start + "[" + endUa + "]{" + endEn + "}";
-                        text.add(fin);
-                    } else if (mat3En.find()) {
-                        endEn = stEn.substring(mat3En.end());
-                        String fin = start + "[" + endUa + "]{" + endEn + "}";
-                        text.add(fin);
+                        createAndAddSting(start, endUa, stEn, mat2En);
                     }
 
                 } else if (mat2.find()) {
                     String start = stUa.substring(0, mat2.end());
                     String endUa = stUa.substring(mat2.end());
                     if (mat1En.find()) {
-                        endEn = stEn.substring(mat1En.end());
-                        String fin = start + "[" + endUa + "]{" + endEn + "} ";
-                        text.add(fin);
+                        createAndAddSting(start, endUa, stEn, mat1En);
                     } else if (mat2En.find()) {
-                        endEn = stEn.substring(mat2En.end());
-                        String fin = start + "[" + endUa + "]{" + endEn + "} ";
-                        text.add(fin);
-                    } else if (mat3En.find()) {
-                        endEn = stEn.substring(mat3En.end());
-                        String fin = start + "[" + endUa + "]{" + endEn + "} ";
-                        text.add(fin);
+                        createAndAddSting(start, endUa, stEn, mat2En);
                     }
                 } else { //если что то не так
                     String fin = "!" + stUa + "}{" + stEn + "}";
                     text.add(fin);
                 }
-
             }
         }
+    }
 
+    private void createAndAddSting(String start, String endUa, String string, Matcher matcher) {
+        String end;
+        end = string.substring(matcher.end());
+        String fin = start + "[" + endUa + "]{" + end + "}";
+        text.add(fin);
     }
 
     @Override
